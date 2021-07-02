f232919254c19edc7f87bfd7c0740cb3960e9c7e
divya-k-nair <divyakaladharannair@gmail.com>
2021-07-02 11:58:20 +0530

    project completed

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main()
+{
+  runApp(App());
+}
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+    appBar:AppBar(
+      title: Text("my flutter app"),
+),
+body: Container(
+child: Column(
+children: [
+  Text("hai 1"),
+  Text("hai 2"),
+  Text("hai 3")
+],
+
+),
+),
+    ),
+    );
+  }
+}
+
+
